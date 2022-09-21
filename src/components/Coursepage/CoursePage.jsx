ee8dde8992e0e9fbda39cbf0689e0d251c6312f8
Vikas Jaiswar <99990935+vikasjsr@users.noreply.github.com>
2022-09-22 00:54:15 +0530

    Update CoursePage.jsx

--- src/components/Coursepage/CoursePage.jsx
+++ src/components/Coursepage/CoursePage.jsx
@@ -1,19 +1,13 @@
 import { Box, Grid, Heading, Text, VStack } from '@chakra-ui/react';
- import React, { useEffect, useState } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { Navigate, useParams } from 'react-router-dom';
+ import React, { useState } from 'react';
+
 
 const CoursePage = () => {
     const [lectureNumber, setLectureNumber] = useState(0);
 
     const { lectures, loading } = useSelector(state => state.course);
   
-    // const dispatch = useDispatch();
-    // const params = useParams();
-  
-    // useEffect(() => {
-    //   dispatch(getCourseLectures(params.id));
-    // }, [dispatch, params.id]);
+    
   
     if (
       user.role !== 'admin' &&
